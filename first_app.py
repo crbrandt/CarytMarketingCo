dbd1a11fb7eb52d9cce84e2078ce76b881146e88
crbrandt <83561834+crbrandt@users.noreply.github.com>
2021-05-02 18:01:00 -0700

    Add files via upload

---
+++ first_app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+import streamlit as st
+
+st.write("#Streamlit")
+
+
+st.write("streamlit!")
+
+
+# In[20]:
+
+
+
+
+
+# In[13]:
+
+
+
+
+
+# In[18]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
