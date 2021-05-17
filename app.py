b2cd24e7137b7f640a7d9075a61466f8bf26151c
crbrandt <83561834+crbrandt@users.noreply.github.com>
2021-05-16 18:10:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -147,11 +147,7 @@ with about:
     Thank you for visiting the Super Bowl Advertisement Optimizer, powered by Caryt Marketing Co. For more information, please visit our team's [Github repository] (https://github.com/crbrandt/CarytMarketingCo).
     
     Curated by the Caryt Marketing Co. Analytics team: \n
-    Cole Brandt \n
-    Anton Averin \n
-    Ranaa Ansari \n
-    Young Jang \n
-    Tyrone Brown
+    Cole Brandt, Anton Averin, Ranaa Ansari, Young Jang, Tyrone Brown
     '''
     
 st.image("https://i.ibb.co/9qDzx87/Sunrise-Abstract-Shapes-Logo-Template-copy.png",
