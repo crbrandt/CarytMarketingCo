32a83b6c0681a362e0da3b25d99bee9197bce954
crbrandt <83561834+crbrandt@users.noreply.github.com>
2021-05-20 22:22:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -198,7 +198,7 @@ moods = st.multiselect(
     [x for x in mood_map.keys()]
     )
 
-if moods.len > 3:
+if len(moods) > 3:
   st.write('Please limit your mood selections to three or less.')
 
 celebs = st.multiselect(
@@ -212,7 +212,7 @@ for celeb in celebs:
     model_inputs[celeb_map[celeb]] = celeb_slider
     
     
-if moods.len <= 3:
+if len(moods) <= 3:
   button = st.button('Predict')
   
 best_ad_link = worst_ad_link = best_ad_title = worst_ad_title = best_ad_score = worst_ad_score = ''
