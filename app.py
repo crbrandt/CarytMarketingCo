27d28fefe0073787fb8a1662d26d6ab72b3ae579
crbrandt <83561834+crbrandt@users.noreply.github.com>
2021-05-20 21:04:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ st.set_page_config(page_title='Caryt Marketing Co. Super Bowl Advertisement Anal
 
 def highlight(text):
      st.markdown(f'<p style="text-align: center;color:#f19e28;font-size:22px;border-radius:2%;">{text}</p>', unsafe_allow_html=True)
-
+def color(text):
+     st.markdown(f'<p style="color:#f19e28;font-size:16px;border-radius:2%;">{text}</p>', unsafe_allow_html=True)
 
 # streamlit run C:\Users\brown\Desktop\MSDS\Capstone\predict.py
 
@@ -273,13 +274,13 @@ if button:
     st.write('(CLUSTER DESCRIPTION)')
     col1, col2 = st.beta_columns(2)
     with col1:
-      st.markdown(f'## Ad from this cluster which performed well:')
-      highlight(f'{best_ad_title}')
+      st.markdown(f'### Ad from this cluster which performed well:')
+      color(f'{best_ad_title}')
       st.write(f'Ad Score: {best_ad_score}')
       st.video(best_ad_link)
     with col2:
-      st.markdown(f'## Ad from this cluster which performed poorly:')
-      highlight(f'{worst_ad_title}')
+      st.markdown(f'### Ad from this cluster which performed poorly:')
+      color(f'{worst_ad_title}')
       st.write(f'Ad Score: {worst_ad_score}')
       st.video(worst_ad_link)
 else:
