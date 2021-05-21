2578796da639815d5ee7e1e3149b49064235e297
crbrandt <83561834+crbrandt@users.noreply.github.com>
2021-05-20 22:24:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -213,165 +213,165 @@ for celeb in celebs:
     
     
 if len(moods) <= 3:
-  button = st.button('Predict')
-  
-best_ad_link = worst_ad_link = best_ad_title = worst_ad_title = best_ad_score = worst_ad_score = ''
+    button = st.button('Predict')
+
+    best_ad_link = worst_ad_link = best_ad_title = worst_ad_title = best_ad_score = worst_ad_score = ''
 
 
-#cluster_info = st.slider('Cluster Number: ', min_value = 0, max_value = 5)
-cluster_info = 0;
-cluster_text = ''
+    #cluster_info = st.slider('Cluster Number: ', min_value = 0, max_value = 5)
+    cluster_info = 0;
+    cluster_text = ''
 
-if button:
-    result = predict(industry,moods, celeb_sliders)
-    highlight(('Your Predicted Ad Score is {}'.format(result) + ' out of 100'))
-        #
-    #
-    #
-    if industry == 'Beer' and not ('Inspirational' in moods or 'Heartwarming' in moods or 'Dramatic' in moods or 'Somber' in moods or 'Serious' in moods):
-      cluster_info = 5
-    if industry == 'Beer' and ('Inspirational' in moods or 'Heartwarming' in moods or 'Dramatic' in moods or 'Somber' in moods or 'Serious' in moods):
-      cluster_info = 1
-    if industry == 'Cars':
-      cluster_info = 3
-    if industry == 'Cellular, Internet, and TV Providers':
-      cluster_info = 5
-    if industry == 'Restaurants and Fast Food':
-      cluster_info = 2
-    if industry == 'Games':
-      cluster_info = 3
-    if industry == 'Loans':
-      cluster_info = 1
-    if industry == 'Music, Movies, and Entertainment':
-      cluster_info = 2
-    if industry == 'Online Streaming Services':
-      cluster_info = 4
-    if industry == 'Snacks':
-      cluster_info = 2
-    if industry == 'Soft Drinks':
-      cluster_info = 2
-    if industry == 'Software and Technology' and ('Funny' in moods or 'Goofy' in moods or 'Party-themed' in moods):
-      cluster_info = 5
-    if industry == 'Software and Technology' and not ('Funny' in moods or 'Goofy' in moods or 'Party-themed' in moods):
-      cluster_info = 1
-    if industry == 'Virtual Assistants' and ('Funny' in moods or 'Goofy' in moods or 'Party-themed' in moods):
-      cluster_info = 2
-    if industry == 'Virtual Assistants' and not ('Funny' in moods or 'Goofy' in moods or 'Party-themed' in moods):
-      cluster_info = 1
-    if industry == 'Other':
-      cluster_info = 1
-    if industry == 'Sports Leagues' or model_inputs[celeb_map['NFL Players']] > 2:
-      cluster_info = 0
-    #
-    #
-    #
-    if cluster_info == 0:
-      best_ad_title = 'NFL - The 100-Year Game'
-      worst_ad_title = 'Jublia - Best Kept Secret'
-      best_ad_link = 'https://www.youtube.com/watch?v=tJjiIuH1VnY'
-      worst_ad_link = 'https://www.youtube.com/watch?v=AXCCmCwzRPs'
-      best_ad_score = '77 out of 100'
-      worst_ad_score = '32 out of 100'
-      cluster_name = 'Superbowl/NFL Advertisements'
-      cluster_text = """
-        - This cluster had the highest average rating among all the clusters.
-        - Primarily composed of advertisements in regards to the Superbowl or involving NFL actors.
-        - A common theme is the advertisement showcasing some sort of party theme.
-        - Exciting or funny moods are the most likely.
-        """
-    if cluster_info == 1:
-      best_ad_title = 'Budweiser - Lost Dog'
-      worst_ad_title = 'Donald Trump - Criminal Justice Reform'
-      best_ad_link = 'https://www.youtube.com/watch?v=TPKgC8KPBMg'
-      worst_ad_link = 'https://www.youtube.com/watch?v=Xtv_PJE8xns'
-      best_ad_score = '81 out of 100'
-      worst_ad_score = '33 out of 100'
-      cluster_name = '\'The Unique\''
-      cluster_text = """
-        - Largest cluster out of all of them and thus contained the most variety in all the attributes. However this was an indication that this cluster contained the advertisements that were most unique.
-        - While popular industries are present here, this cluster contains industries that are not very popular or common such as sex paraphernalia and politics.
-        - Athletes in general are commonly found as actors here along with politicians and other famous personnel.
-        - The moods vary with the biggest two being funny and exciting, but the variety stretching from light hearted to patriotic to emotional.
-        """
-    if cluster_info == 2:
-      best_ad_title = 'M&M\'S - Bad Passengers'
-      worst_ad_title = 'Burger King - Eat like Andy'
-      best_ad_link = 'https://www.youtube.com/watch?v=w7FIka-jYM8'
-      worst_ad_link = 'https://www.youtube.com/watch?v=HcbWkcDRJjc'
-      best_ad_score = '65 out of 100'
-      worst_ad_score = '46 out of 100'
-      cluster_name = 'Food, Drink, and Movies'
-      cluster_text = """
-        - This Cluster was primarily composed of advertisements for a variety of beverages and snacks. Full meals were not prevalent in general but specifically snacks and drinks were quite common.
-        - This cluster contained advertisements that used the highest amount of actors, both of the well-known fame level and of a lesser level.
-        - This cluster was far more likely to contain some sort of comedian either in the story or as an actor.
-        - Funny and exciting were the predominant moods.
-        """
-    if cluster_info == 3:
-      best_ad_title = 'Kia Niro - Hero\'s Journey'
-      worst_ad_title = 'Lexus LS 500 F SPORT - Blank Panther'
-      best_ad_link = 'https://www.youtube.com/watch?v=pVxmT2x3Od4'
-      worst_ad_link = 'https://www.youtube.com/watch?v=1isIeJy6lTE'
-      best_ad_score = '77 out of 100'
-      worst_ad_score = '51 out of 100'
-      cluster_name = 'Cars, Cars, and More Cars'
-      cluster_text = """
-        - This cluster was predominantly made up of advertisements for specific car models and car manufacturers. There was also a non-insignificant amount of movie  and game focused ads.
-        - This cluster had the most talk show hosts involved as part of the story or in the capacity of an actor.
-        - These advertisements involved the most actors on a number basis (i.e on set and on screen).
-        - Dramatic and adventurous were the most common moods.
-        """
-    if cluster_info == 4:
-      best_ad_title = 'Netflix - Our Planet'
-      worst_ad_title = 'Quibi - Bank Heist'
-      best_ad_link = 'https://www.youtube.com/watch?v=BSLzGgFbHZE'
-      worst_ad_link = 'https://www.youtube.com/watch?v=cBlKObT5dv0'
-      best_ad_score = '61 out of 100'
-      worst_ad_score = '44 out of 100'
-      cluster_name = 'Streaming Services'
-      cluster_text = """
-        - This group is mainly composed of streaming services
-        - There tend to be musicians either involved in the story on screen.
-        - Only category with wrestlers involved in either the story or as actors.
-        - Funny or inspirational moods are the most likely.
-        """
-    if cluster_info == 5:
-      best_ad_title = 'Bud Light - Special Delivery'
-      worst_ad_title = 'Michelob Ultra - Breathe'
-      best_ad_link = 'https://www.youtube.com/watch?v=JZC3K6qk4wY'
-      worst_ad_link = 'https://www.youtube.com/watch?v=s4A_BsWnY8w'
-      best_ad_score = '61 out of 100'
-      worst_ad_score = '41 out of 100'
-      cluster_name = 'Chatting with Beer'
-      cluster_text = """
-        - This cluster was composed of two industries primarily, the alcohol industry with a focus on beer, and on the mobile providers and new phone models.
-        - Contained one of the smallest amounts of well-known actors, and in general, used very few actors on set.
-        - Following along with the beer grouping, this cluster also had a high amount of potato chip advertisements paired with it.
-        - The predominant mood was funny by a very large margin with goofy and adorable being the only two significant moods following it.
-        """
-    st.markdown(f'### Cluster Profile: {cluster_name}')
-    st.write(cluster_text)
-    col1, col2 = st.beta_columns(2)
-    with col1:
-      st.markdown('### Ad from this cluster which performed well:')
-      color(f'{best_ad_title}')
-      st.write(f'Ad Score: {best_ad_score}')
-      st.video(best_ad_link)
-    with col2:
-      st.markdown('### Ad from this cluster which performed poorly:')
-      color(f'{worst_ad_title}')
-      st.write(f'Ad Score: {worst_ad_score}')
-      st.video(worst_ad_link)
-else:
-    st.write("#")
-    st.write("#")
-    st.write("#")
-    st.write("#")
-    st.write("#")
-    st.write("#")
-    st.write("#")
-    st.write("#")
-    st.write("#")
+    if button:
+      result = predict(industry,moods, celeb_sliders)
+      highlight(('Your Predicted Ad Score is {}'.format(result) + ' out of 100'))
+          #
+      #
+      #
+      if industry == 'Beer' and not ('Inspirational' in moods or 'Heartwarming' in moods or 'Dramatic' in moods or 'Somber' in moods or 'Serious' in moods):
+        cluster_info = 5
+      if industry == 'Beer' and ('Inspirational' in moods or 'Heartwarming' in moods or 'Dramatic' in moods or 'Somber' in moods or 'Serious' in moods):
+        cluster_info = 1
+      if industry == 'Cars':
+        cluster_info = 3
+      if industry == 'Cellular, Internet, and TV Providers':
+        cluster_info = 5
+      if industry == 'Restaurants and Fast Food':
+        cluster_info = 2
+      if industry == 'Games':
+        cluster_info = 3
+      if industry == 'Loans':
+        cluster_info = 1
+      if industry == 'Music, Movies, and Entertainment':
+        cluster_info = 2
+      if industry == 'Online Streaming Services':
+        cluster_info = 4
+      if industry == 'Snacks':
+        cluster_info = 2
+      if industry == 'Soft Drinks':
+        cluster_info = 2
+      if industry == 'Software and Technology' and ('Funny' in moods or 'Goofy' in moods or 'Party-themed' in moods):
+        cluster_info = 5
+      if industry == 'Software and Technology' and not ('Funny' in moods or 'Goofy' in moods or 'Party-themed' in moods):
+        cluster_info = 1
+      if industry == 'Virtual Assistants' and ('Funny' in moods or 'Goofy' in moods or 'Party-themed' in moods):
+        cluster_info = 2
+      if industry == 'Virtual Assistants' and not ('Funny' in moods or 'Goofy' in moods or 'Party-themed' in moods):
+        cluster_info = 1
+      if industry == 'Other':
+        cluster_info = 1
+      if industry == 'Sports Leagues' or model_inputs[celeb_map['NFL Players']] > 2:
+        cluster_info = 0
+      #
+      #
+      #
+      if cluster_info == 0:
+        best_ad_title = 'NFL - The 100-Year Game'
+        worst_ad_title = 'Jublia - Best Kept Secret'
+        best_ad_link = 'https://www.youtube.com/watch?v=tJjiIuH1VnY'
+        worst_ad_link = 'https://www.youtube.com/watch?v=AXCCmCwzRPs'
+        best_ad_score = '77 out of 100'
+        worst_ad_score = '32 out of 100'
+        cluster_name = 'Superbowl/NFL Advertisements'
+        cluster_text = """
+          - This cluster had the highest average rating among all the clusters.
+          - Primarily composed of advertisements in regards to the Superbowl or involving NFL actors.
+          - A common theme is the advertisement showcasing some sort of party theme.
+          - Exciting or funny moods are the most likely.
+          """
+      if cluster_info == 1:
+        best_ad_title = 'Budweiser - Lost Dog'
+        worst_ad_title = 'Donald Trump - Criminal Justice Reform'
+        best_ad_link = 'https://www.youtube.com/watch?v=TPKgC8KPBMg'
+        worst_ad_link = 'https://www.youtube.com/watch?v=Xtv_PJE8xns'
+        best_ad_score = '81 out of 100'
+        worst_ad_score = '33 out of 100'
+        cluster_name = '\'The Unique\''
+        cluster_text = """
+          - Largest cluster out of all of them and thus contained the most variety in all the attributes. However this was an indication that this cluster contained the advertisements that were most unique.
+          - While popular industries are present here, this cluster contains industries that are not very popular or common such as sex paraphernalia and politics.
+          - Athletes in general are commonly found as actors here along with politicians and other famous personnel.
+          - The moods vary with the biggest two being funny and exciting, but the variety stretching from light hearted to patriotic to emotional.
+          """
+      if cluster_info == 2:
+        best_ad_title = 'M&M\'S - Bad Passengers'
+        worst_ad_title = 'Burger King - Eat like Andy'
+        best_ad_link = 'https://www.youtube.com/watch?v=w7FIka-jYM8'
+        worst_ad_link = 'https://www.youtube.com/watch?v=HcbWkcDRJjc'
+        best_ad_score = '65 out of 100'
+        worst_ad_score = '46 out of 100'
+        cluster_name = 'Food, Drink, and Movies'
+        cluster_text = """
+          - This Cluster was primarily composed of advertisements for a variety of beverages and snacks. Full meals were not prevalent in general but specifically snacks and drinks were quite common.
+          - This cluster contained advertisements that used the highest amount of actors, both of the well-known fame level and of a lesser level.
+          - This cluster was far more likely to contain some sort of comedian either in the story or as an actor.
+          - Funny and exciting were the predominant moods.
+          """
+      if cluster_info == 3:
+        best_ad_title = 'Kia Niro - Hero\'s Journey'
+        worst_ad_title = 'Lexus LS 500 F SPORT - Blank Panther'
+        best_ad_link = 'https://www.youtube.com/watch?v=pVxmT2x3Od4'
+        worst_ad_link = 'https://www.youtube.com/watch?v=1isIeJy6lTE'
+        best_ad_score = '77 out of 100'
+        worst_ad_score = '51 out of 100'
+        cluster_name = 'Cars, Cars, and More Cars'
+        cluster_text = """
+          - This cluster was predominantly made up of advertisements for specific car models and car manufacturers. There was also a non-insignificant amount of movie  and game focused ads.
+          - This cluster had the most talk show hosts involved as part of the story or in the capacity of an actor.
+          - These advertisements involved the most actors on a number basis (i.e on set and on screen).
+          - Dramatic and adventurous were the most common moods.
+          """
+      if cluster_info == 4:
+        best_ad_title = 'Netflix - Our Planet'
+        worst_ad_title = 'Quibi - Bank Heist'
+        best_ad_link = 'https://www.youtube.com/watch?v=BSLzGgFbHZE'
+        worst_ad_link = 'https://www.youtube.com/watch?v=cBlKObT5dv0'
+        best_ad_score = '61 out of 100'
+        worst_ad_score = '44 out of 100'
+        cluster_name = 'Streaming Services'
+        cluster_text = """
+          - This group is mainly composed of streaming services
+          - There tend to be musicians either involved in the story on screen.
+          - Only category with wrestlers involved in either the story or as actors.
+          - Funny or inspirational moods are the most likely.
+          """
+      if cluster_info == 5:
+        best_ad_title = 'Bud Light - Special Delivery'
+        worst_ad_title = 'Michelob Ultra - Breathe'
+        best_ad_link = 'https://www.youtube.com/watch?v=JZC3K6qk4wY'
+        worst_ad_link = 'https://www.youtube.com/watch?v=s4A_BsWnY8w'
+        best_ad_score = '61 out of 100'
+        worst_ad_score = '41 out of 100'
+        cluster_name = 'Chatting with Beer'
+        cluster_text = """
+          - This cluster was composed of two industries primarily, the alcohol industry with a focus on beer, and on the mobile providers and new phone models.
+          - Contained one of the smallest amounts of well-known actors, and in general, used very few actors on set.
+          - Following along with the beer grouping, this cluster also had a high amount of potato chip advertisements paired with it.
+          - The predominant mood was funny by a very large margin with goofy and adorable being the only two significant moods following it.
+          """
+      st.markdown(f'### Cluster Profile: {cluster_name}')
+      st.write(cluster_text)
+      col1, col2 = st.beta_columns(2)
+      with col1:
+        st.markdown('### Ad from this cluster which performed well:')
+        color(f'{best_ad_title}')
+        st.write(f'Ad Score: {best_ad_score}')
+        st.video(best_ad_link)
+      with col2:
+        st.markdown('### Ad from this cluster which performed poorly:')
+        color(f'{worst_ad_title}')
+        st.write(f'Ad Score: {worst_ad_score}')
+        st.video(worst_ad_link)
+    else:
+      st.write("#")
+      st.write("#")
+      st.write("#")
+      st.write("#")
+      st.write("#")
+      st.write("#")
+      st.write("#")
+      st.write("#")
+      st.write("#")
 
     
 
