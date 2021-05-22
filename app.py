2c5f5cbd518d7a45a32a9da2c7136cf9282d1bd6
crbrandt <83561834+crbrandt@users.noreply.github.com>
2021-05-22 11:22:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -411,7 +411,7 @@ if len(moods) <= 3 and len(industry) <= 1:
       st.write("#")
       st.write("#")
 
-if len(moods)> 3 or len(industry) > 1:
+if len(moods)> 3 or len(industry) != 1:
       st.write("#")
       st.write("#")
       st.write("#")
